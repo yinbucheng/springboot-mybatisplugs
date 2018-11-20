27168094d442a5aadbc03ed54377a18cfb0e9c72
yinchong <yin.chong@intellif.com>
2018-11-20 16:55:19 +0800

    分支合并

--- src/main/java/cn/bucheng/springmybatis/test/Test.java
+++ src/main/java/cn/bucheng/springmybatis/test/Test.java
@@ -6,4 +6,7 @@ package cn.bucheng.springmybatis.test;
  * @Date 2018/11/7 20:56
  **/
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是一个分支合并测试");
+    }
 }
