e3974780ae8d2e2552c79d2a67b76d27de0521f3
yinchong <yin.chong@intellif.com>
2019-06-18 21:12:16 +0800

    添加redis测试

--- src/test/java/cn/bucheng/redis/RedisTest.java
+++ src/test/java/cn/bucheng/redis/RedisTest.java
@@ -1,5 +1,9 @@
 package cn.bucheng.redis;
 
+import org.junit.Before;
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
 /**
  * @author ：yinchong
  * @create ：2019/6/18 19:16
@@ -8,4 +12,67 @@ package cn.bucheng.redis;
  * @version:
  */
 public class RedisTest {
+
+    private Jedis jedis;
+
+    @Before
+    public void before() {
+        //连接本地的 Redis 服务
+        jedis = new Jedis("localhost");
+    }
+
+    @Test
+    public void testSetString(){
+        String set = jedis.set("name", "yinchong");
+        System.out.println(set);
+    }
+
+
+    @Test
+    public void testGetString(){
+        String name = jedis.get("name");
+        System.out.println(name);
+    }
+
+    @Test
+    public void testExpireKey(){
+        Long time = jedis.expire("name", 10);
+        System.out.println(time);
+    }
+
+
+    @Test
+    public void testTtlKey(){
+        Long time = jedis.ttl("name");
+        System.out.println(time);
+    }
+
+    @Test
+    public void testSetAge(){
+        String row = jedis.set("age", "20");
+        System.out.println(row);
+    }
+
+    @Test
+    public void testDelete(){
+        Long row = jedis.del("age");
+        System.out.println(row);
+    }
+
+    @Test
+    public void testIncr(){
+        Long number = jedis.incr("age");
+        System.out.println(number);
+    }
+
+    @Test
+    public void testIncrBy(){
+        Long number = jedis.incrBy("age", 10);
+        System.out.println(number);
+    }
+
+    @Test
+    public void testCountKey(){
+
+    }
 }
