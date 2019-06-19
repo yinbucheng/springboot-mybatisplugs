547ee290fb48ca514c6bd22494308678a73f85df
yinchong <yin.chong@intellif.com>
2019-06-19 09:34:02 +0800

    添加fastdfs测试代码

--- src/test/java/cn/bucheng/fastdfs/FastDFSTest.java
+++ src/test/java/cn/bucheng/fastdfs/FastDFSTest.java
@@ -1,5 +1,10 @@
 package cn.bucheng.fastdfs;
 
+import com.github.tobato.fastdfs.service.DefaultFastFileStorageClient;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * @author ：yinchong
  * @create ：2019/6/18 19:16
@@ -8,4 +13,11 @@ package cn.bucheng.fastdfs;
  * @version:
  */
 public class FastDFSTest {
+
+    private FastFileStorageClient fileStorageClient;
+
+    @Before
+    public void init(){
+        fileStorageClient = new DefaultFastFileStorageClient();
+    }
 }
