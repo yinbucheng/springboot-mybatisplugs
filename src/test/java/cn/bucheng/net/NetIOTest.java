fbbcca8eaf071de9c56a67cfeff367c6545803b9
yinchong <yin.chong@intellif.com>
2019-06-21 17:39:58 +0800

    添加网络测试代码

--- src/test/java/cn/bucheng/net/NetIOTest.java
+++ src/test/java/cn/bucheng/net/NetIOTest.java
@@ -18,9 +18,9 @@ public class NetIOTest {
 
     @Test
     public void testClientTimeWait() throws IOException {
-        try(Socket client = new Socket()) {
+        try (Socket client = new Socket()) {
             client.bind(new InetSocketAddress(9988));
-            client.setSoLinger(true,0);
+            client.setSoLinger(true, 0);
             client.connect(new InetSocketAddress("127.0.0.1", 9090));
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
             BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
@@ -58,8 +58,8 @@ public class NetIOTest {
 
 
     @Test
-    public void testClient()throws Exception {
-        try(Socket client = new Socket()) {
+    public void testClient() throws Exception {
+        try (Socket client = new Socket()) {
             client.bind(new InetSocketAddress(9008));
             client.connect(new InetSocketAddress("127.0.0.1", 9090));
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
@@ -70,8 +70,47 @@ public class NetIOTest {
         }
     }
 
+    @Test
+    public void testFINWAIT2Client() throws Exception {
+        Socket socket = new Socket();
+        socket.bind(new InetSocketAddress(9099));
+        socket.connect(new InetSocketAddress("127.0.1", 9090));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        for (int i = 0; i < 20; i++) {
+            writer.println("this is message from client, this number is " + i);
+            writer.flush();
+            Thread.sleep(2000);
+        }
+        socket.shutdownOutput();
+        String line;
+        while((line=reader.readLine())!=null){
+            System.out.println(line);
+        }
+        socket.close();
+    }
 
-
-
+    @Test
+    public void testCLOSEWAITServer() throws Exception {
+        ServerSocket serverSocket = new ServerSocket();
+        serverSocket.bind(new InetSocketAddress(9090));
+        while (true) {
+            Socket socket = serverSocket.accept();
+            System.out.println("accept new client");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            String line;
+            while((line= reader.readLine())!=null){
+                System.out.println(line);
+            }
+            for (int i = 0; i < 20; i++) {
+                writer.println("this message from server,this number is "+i);
+                writer.flush();
+                Thread.sleep(2000);
+            }
+            socket.shutdownOutput();
+            socket.close();
+        }
+    }
 
 }
