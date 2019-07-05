45dd2c3bb11f0c6743b14f63eba57b5029925dfb
yinchong <yin.chong@intellif.com>
2019-07-05 12:32:28 +0800

    添加拦截器和过滤器的测试使用

--- src/main/java/cn/bucheng/BootstrapApplication.java
+++ src/main/java/cn/bucheng/BootstrapApplication.java
@@ -6,17 +6,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"cn.bucheng.authmanager.dao"})
 @Import(FdfsClientConfig.class)
+@RestController
 public class BootstrapApplication {
 
     public static void main(String[] args) {
         System.setProperty("es.set.netty.runtime.available.processors", "false");
         SpringApplication.run(BootstrapApplication.class, args);
+    }
+
+
+    @RequestMapping("/test2")
+    public Object test(){
+        return "test";
     }
 
 
---
+++ src/test/java/cn/bucheng/springboot/web/WebBootApplicationTest.java
@@ -0,0 +1,19 @@
+package cn.bucheng.springboot.web;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author ：yinchong
+ * @create ：2019/7/5 10:31
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebBootApplicationTest {
+
+
+}
